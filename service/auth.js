86d392d160318854b1c273abd9d4aa13d4d3755e
Reis <nathan.david@siriusgroup.com.br>
2024-04-24 21:31:16 -0300

    Cadastro quase pronto
    Falta ajustes

--- service/auth.js
+++ service/auth.js
@@ -1,4 +1,4 @@
-const createUser = async (email, password,name) => {
+const createUser = async (email,password,name) => {
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(credential => {
         return firebase.firestore().collection('user').doc(credential.user.uid).set({
