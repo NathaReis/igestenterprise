7723fa5d3df4eec7f58ee343a6aff4ab616031de
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-05-02 13:53:17 -0300

    crio, edito e deleto elementos da escala

--- js/calendario.js
+++ js/calendario.js
@@ -8,6 +8,7 @@ const nowYear = new Date().getFullYear()
 let yearCurrent = new Date().getFullYear()
 let monthCurrent = new Date().getMonth()
 let dateCurrent
+let isUpdateForm = false
 
 const clearClassDays = () => {
     $daysPosition.forEach(day => {
@@ -51,11 +52,7 @@ const getDates = () => {
     clearClassDays()
     $year.forEach(year => year.innerHTML = yearCurrent)
     $month.forEach(month => month.innerHTML = monthNumberForMonthName(monthCurrent))
-    if(yearCurrent == nowYear && monthCurrent == nowMonth) {
-        const nowDay = new Date().getDate()
-        $daysPosition[nowDay].classList.add("currentDay")
-    }
-    
+
     const data = numberDaysMonth()
     let count = 0
     let countLast = 0
@@ -82,6 +79,11 @@ const getDates = () => {
         $daysPosition[i].innerHTML = data.lastDays
         data.lastDays--
     }// Last days
+
+    if(yearCurrent == nowYear && monthCurrent == nowMonth) {
+        const nowDay = new Date().getDate() + countLast - 1
+        $daysPosition[nowDay].classList.add("currentDay")
+    }// Current day
 }
 getDates()
 
@@ -242,7 +244,8 @@ $daysPosition.forEach(day => {
 })
 
 // ONCLICK DAY FOR LISTEVENTS
-const transitionPages = (page) => {  
+const transitionPages = (page, update=false) => { 
+    isUpdateForm = update // Para saber o que fazer ao entrar no form e finalizar 
     const $calendario = document.querySelector("#calendario")  
     const $listEvents = document.querySelector("#listEvents")  
     const $form = document.querySelector("#form")  
@@ -350,6 +353,7 @@ form.start_hour.addEventListener("change", () => {
 //Escala Add List
 function preencherLista() {
     const container = document.querySelector("#listEscalaAddList")
+    container.innerHTML = ""
     for(escala of form.escala) {
         const card = document.createElement("div")
         card.classList.add("card")
@@ -388,6 +392,9 @@ function preencherLista() {
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
         </svg>`
+        edit.onclick = () => {
+            editEscala(escala)
+        }
         buttons.appendChild(edit)
 
         const del = document.createElement("button")
@@ -395,12 +402,21 @@ function preencherLista() {
         del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
     </svg>`
+        del.onclick = () => {
+            dialog.showConfirmMessage("Exclusão","Deseja excluir o componente?","center")
+            .then(() => {
+                const newList = form.escala.filter(element => element != escala)
+                form.escala = newList
+                preencherLista()
+            })
+            .catch(error => console.log(error))
+        }
         buttons.appendChild(del)
 
         options.appendChild(buttons)
-        card.appendChild(options)
-
+        
         card.appendChild(dados)
+        card.appendChild(options)
         container.appendChild(card)
     }
 }
@@ -429,12 +445,27 @@ $escalaAdd.addEventListener("submit", (e) => {
         hour: $escalaHour.value,
     }
 
-    form.escala.push(escala)
+    if(isUpdateForm) {
+        const newList = form.escala.filter(element => element != isUpdateForm)
+        newList.push(escala)
+        form.escala = newList
+    }
+    else {
+        form.escala.push(escala)
+    }
     
-    resetEscalaAddComponents()
-
+    resetEscalaAddComponents()        
     transitionPages("#escalaAddList")
 })
+
+// Escala update
+const editEscala = (escala) => {
+    $option.value = escala.function
+    $namesEscala.value = escala.names
+    $escalaHour.value = escala.hour
+    $retornoOption.innerHTML = `${escala.function.toString().toUpperCase()}`
+    transitionPages("#escalaAdd", escala)
+}
 
 // Select
 const toggleOptionsEscala = () => {
